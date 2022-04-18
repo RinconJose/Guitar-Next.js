a407663fadcb6261958b31a160d6c01dade8e5a6
Jose Rincon <jose@150porciento.com>
2022-04-18 08:15:33 -0500

    creando todas las vistas, estilos generales, header, footer, y ajustando el proyecto en general

---
+++ pages/tienda.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+const Tienda = () => {
+  return (
+    <Layout
+        pagina='Tienda Virtul'
+    >
+        <h1>Desde tienda virtual</h1>
+    </Layout>
+  )
+}
+
+export default Tienda
